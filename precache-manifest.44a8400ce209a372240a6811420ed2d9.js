fe7f87cbbbc9c698571c1846cdd800dac7884b49
Abhilash Kulkarni <abhikul1993@gmail.com>
2020-04-03 15:51:22 -0400

    Updates

---
+++ precache-manifest.44a8400ce209a372240a6811420ed2d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035a7fa05e0d8461781e6ef8f5e7f59c",
+    "url": "/create-react-app/app/index.html"
+  },
+  {
+    "revision": "c37e21c2375d33c8c902",
+    "url": "/create-react-app/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "41bd30bd5affef7fbe41",
+    "url": "/create-react-app/app/static/js/2.8bc0949e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/app/static/js/2.8bc0949e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37e21c2375d33c8c902",
+    "url": "/create-react-app/app/static/js/main.c4cd9368.chunk.js"
+  },
+  {
+    "revision": "842b22771f401f236f2e",
+    "url": "/create-react-app/app/static/js/runtime-main.449db7f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
